f412d092b11e912ba53fba74b9a6d9227a153276
sohseyedi <sohseyedi@gmail.com>
2024-11-24 21:54:16 +0330

    empty component

---
+++ components/Empty.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { hp } from '../utils/common'
+
+const Empty = () => {
+    return (
+        <View style={styles.empty}>
+            <Text style={styles.text}>Nothing Transactions</Text>
+        </View>
+    )
+}
+
+export default Empty
+
+const styles = StyleSheet.create({
+    empty: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text:{
+        fontSize:hp(2.2),
+        fontWeight:"semibold",
+        color:"#fefefe"
+    }
+})
