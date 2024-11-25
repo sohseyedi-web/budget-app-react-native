18fcea66e8da1fe5a645a4aaed294b72d9952753
sohseyedi <sohseyedi@gmail.com>
2024-11-25 11:54:37 +0330

    display icons based on transaction types

--- components/IconBox.jsx
+++ components/IconBox.jsx
@@ -1,9 +1,14 @@
 import { StyleSheet, View } from 'react-native'
 import { hp } from '../utils/common'
+import { ArrowDownTrayIcon, ArrowUpTrayIcon, CircleStackIcon } from 'react-native-heroicons/outline';
 
-const IconBox = ({ icon, type }) => {
+const IconBox = ({ type }) => {
 
-    const isType = type === "income" ? "#4335A7" : "#D91656"
+    const isType = type === "income" ? "#4335A7" : type === "expense" ? "#D91656" : "#141414";
+    const icon =
+        type === "income" ? <ArrowUpTrayIcon size={24} color={"#fff"} /> :
+            type === "expense" ? <ArrowDownTrayIcon size={24} color={"#fff"} /> :
+                <CircleStackIcon size={24} color={"#fff"} />;
 
     return (
         <View style={[styles.box, { backgroundColor: isType }]}>
