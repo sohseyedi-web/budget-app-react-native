fe583d16c80797538c70627f1cabcc680063eb86
sohseyedi <sohseyedi@gmail.com>
2024-11-25 11:54:01 +0330

    center epmty text

--- components/Empty.jsx
+++ components/Empty.jsx
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        minHeight:"80vh"
     },
     text:{
         fontSize:hp(2.2),
