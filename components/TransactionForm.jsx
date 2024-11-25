9603e15eb85ba56638ddf5e0f82fbdd3ec29dfc3
sohseyedi <sohseyedi@gmail.com>
2024-11-25 11:55:58 +0330

    useTransaction hook into TransactionForm for handling form submissions

--- components/TransactionForm.jsx
+++ components/TransactionForm.jsx
@@ -1,45 +1,29 @@
 import { StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { hp } from '../utils/common'
-import RadioInputGroup from './RadioInputGroup'
 import ButtonAction from './ButtonAction'
 import TextField from './TextField'
-import { useBudget } from '../context/BudgetProvider'
+import { useTransaction } from '../hooks/useTransaction'
+import SelectTypes from './SelectTypes'
 
 const TransactionForm = ({ onClose }) => {
 
     const [amount, setAmount] = useState('')
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState("")
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [loading, setLoading] = useState(false)
-    const { handleTransactions } = useBudget()
+    const [selectedOption, setSelectedOption] = useState("income");
+    const { loading, onSubmitTransaction } = useTransaction()
 
     const handleAddTransaction = () => {
-        setLoading(true)
-        try {
-            const newItem = {
-                id: Math.ceil(Math.random() * 10000),
-                data: {
-                   title, amount, description, selectedOption
-                },
-                createdAt: new Date().toISOString(),
-            }
-            handleTransactions(newItem)
-            onClose()
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        }
+        onSubmitTransaction(title, amount, description, selectedOption)
+        onClose()
     }
-
 
     return (
         <>
             <TextField value={title} onChange={setTitle} placeHolder={"Title"} inputStyle={{ height: hp(6.6) }} />
             <TextField value={amount} onChange={setAmount} placeHolder={"Amount"} inputStyle={{ height: hp(6.6) }} />
-            <RadioInputGroup selectedOption={selectedOption} setSelectedOption={setSelectedOption} />
+            <SelectTypes select={selectedOption} setSelected={setSelectedOption} />
             <TextField value={description} onChange={setDescription} isMulti={true} placeHolder={"Description"} inputStyle={styles.textArea} />
             <ButtonAction title='Submit' onPress={handleAddTransaction} loading={loading} />
         </>
