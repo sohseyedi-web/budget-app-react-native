4982bd14724f59f4c93fa2dd1c3b701fafdcba3a
sohseyedi <sohseyedi@gmail.com>
2024-11-25 14:19:35 +0330

    change icon type

--- components/IconBox.jsx
+++ components/IconBox.jsx
@@ -6,8 +6,8 @@ const IconBox = ({ type }) => {
 
     const isType = type === "income" ? "#4335A7" : type === "expense" ? "#D91656" : "#141414";
     const icon =
-        type === "income" ? <ArrowUpTrayIcon size={24} color={"#fff"} /> :
-            type === "expense" ? <ArrowDownTrayIcon size={24} color={"#fff"} /> :
+        type === "income" ? <ArrowDownTrayIcon size={24} color={"#fff"} /> :
+            type === "expense" ? <ArrowUpTrayIcon size={24} color={"#fff"} /> :
                 <CircleStackIcon size={24} color={"#fff"} />;
 
     return (
